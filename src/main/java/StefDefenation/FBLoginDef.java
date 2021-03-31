89d6e7fdeb85d0544a4f7bab522b1a6220034975
rajadurairavi <68144876+rajadurairavi@users.noreply.github.com>
2021-03-31 07:57:40 +0530

    Update FBLoginDef.java

--- src/main/java/StefDefenation/FBLoginDef.java
+++ src/main/java/StefDefenation/FBLoginDef.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class FBLoginDef extends TestBase {
-	//FBLoginPage loginpage;
+	FBLoginPage loginpage;
 	
 
 	@Given("^Humnan launch browser and get \"([^\"]*)\"$")
